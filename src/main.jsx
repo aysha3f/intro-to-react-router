2572f33f9c3b810bbb6a045166ba0bee027de601
aysha3f <tahiaysha220@gmail.com>
2025-11-11 21:47:05 +0600

    Understand The Concept of Routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router'
+import Root from './components/Root/root.jsx'
+import Home from './components/Home/Home.jsx'
+import Mobile from './components/Mobile/Mobile.jsx'
+import Laptop from './components/Mobile/Laptop/Laptop.jsx'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>hello from router react</div>
-  }
+ {
+   path: '/',
+ Component: Root,
+ children:[
+  {index: true, Component: Home},
+  {path: 'mobiles', Component: Mobile},
+  {path: 'laptops', Component:Laptop}
+ ]
+},
+{
+  path: 'about',
+  element: <div>About me here</div>
+},
+{
+  path:'blogs',
+  element: <div>All my blogs are here</div>
+},
+{
+  path: 'app',
+  Component: App
+},
+{
+  path: '/app2',
+  // element: <App></App>
+  Component: App
+}
 ])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+ <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
 
