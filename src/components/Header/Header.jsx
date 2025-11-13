7adf88faa0f8c481d5e5e286c0cce8ec6e84864e
aysha3f <tahiaysha220@gmail.com>
2025-11-13 21:34:23 +0600

    understand Nested Routing , Outlet , Link and NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link, NavLink } from 'react-router';
+import './Header.css';
 const Header = () => {
     return (
         <div>
             <h3>This is header</h3>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/mobiles">Mobile</NavLink>
+                <NavLink to="/laptops">Laptop</NavLink>
+            </nav>
         </div>
     );
 };
