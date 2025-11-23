cb120507ef8e1d55f7b5079385118a05bde1f4d6
aysha3f <tahiaysha220@gmail.com>
2025-11-23 22:33:56 +0600

    Load data in the component without route and Not Found 404 route

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Suspense, useState } from 'react';
 import { Link } from 'react-router';
+import UserDetails from '../UserDetails/UserDetails';
+import UserDetails2 from '../UserDetails2/UserDetails2';
 
 const User = ({user}) => {
-   const {id, name, email, phone} = user;
+    const [showInfo, setShowInfo] = useState(false);
+    const {id, name, email, phone} = user;
    
+  
+const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=> res.json());
+
+  
    const userStyle ={
     border: '2px solid pink',
     borderRadius: '20px',
@@ -18,6 +25,12 @@ const User = ({user}) => {
             <p>Email: {email}</p>
             <p><small>Phone: {phone}</small></p>
             <Link to={`/users/${id}`}>Show Details</Link>
+            <button onClick={() => setShowInfo(!showInfo)}>{ showInfo? 'Hide' : 'Show'} info</button>
+            {
+                showInfo && <Suspense fallback={<span>Loading....</span>}>
+            <UserDetails2 userPromise={userPromise}></UserDetails2>
+                </Suspense>
+            }
         </div>
     );
 };
